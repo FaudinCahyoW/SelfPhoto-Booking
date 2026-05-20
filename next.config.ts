bae957cf8e65dc99920217bd442c013f2d984e3e
unknown <faudincw@gmail.com>
2026-05-20 18:45:54 +0700

    fix: kunci workspace root turbopack dan rapikan inisialisasi supabase

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import withFlowbiteReact from "flowbite-react/plugin/nextjs";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  turbopack: {
+    root: __dirname, // ✨ Mengunci folder proyek fotografi Anda sebagai root utama Turbopack
+  },
 };
 
 export default withFlowbiteReact(nextConfig);
