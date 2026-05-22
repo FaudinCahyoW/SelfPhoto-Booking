9a8dbe886f0eb20f1cf42810dd82a738c5e62e79
unknown <faudincw@gmail.com>
2026-05-22 08:39:23 +0700

    Change background color

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -27,33 +27,39 @@ export default function ProductCard() {
 
   return (
     <>
-      <div className="mx-aut grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5">
+      <div className="mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5">
         {services.map((b) => (
-          <Card
+          <div
             key={b.service_id}
-            className="bg-linear-to-r from-[#243B55] to-[#141E30] px-8 py-8 text-whit rounded-2xl w-full max-w-sm flex flex-col items-center justify-between"
+            className=" overflow-hidden rounded-3xl bg-card border border-white/30 shadow-xl transition hover:scale-[1.02] flex flex-col h-full "
           >
-            <section className="flex justify-center items-center">
-              <img
-                src={b.service_img}
-                alt={b.service_type}
-                className="w-48 aspectt-2/3 object-cover items-center justify-center"
-              />
-            </section>
-            <section className="mt-3 sm:mt-2 md:mt-3">
-              <a href="#">
-                <h5 className="text-xl text-heading font-semibold tracking-tight text-white">
-                  {b.service_type}
-                </h5>
-              </a>
-              <p className="text-gray-300">{b.service_desc}</p>
-              <div className="flex items-center justify-between mt-3">
-                <span className="text-base sm:text-xl font-extrabold text-heading text-violet-200">
+            {/* Image */}
+            <img
+              className="w-full h-56 object-cover"
+              src={b.service_img}
+              alt={b.service_type}
+            />
+
+            {/* Content */}
+            <div className="p-6 text-black flex flex-col flex-1">
+              <h5 className="mb-3 text-2xl font-semibold tracking-tight">
+                {b.service_type}
+              </h5>
+
+              {/* Description */}
+              <p className="mb-6 text-zinc-700 line-clamp-3">
+                {b.service_desc}
+              </p>
+
+              {/* Bottom section */}
+              <div className="flex items-center justify-between mt-auto">
+                <span className="text-2xl font-extrabold">
                   Rp {b.price_service.toLocaleString("id-ID")}
                 </span>
+
                 <button
                   type="button"
-                  className="w-24 sm:w-24 text-sm rounded cursor-pointer active:bg-green-500 bg-pink-500 hover:bg-pink-400 bg-brand hover:bg-brand-strong box-border border border-transparent focus:ring-4 focus:ring-brand-medium shadow-xs font-medium  rounded-base px-3 py-2 focus:outline-none"
+                  className="bg-orange-500 hover:bg-orange-400 cursor-pointer text-white font-semibold rounded-xl px-5 py-2.5 shadow-lg"
                   onClick={() => {
                     setSelectedService(b);
                     setIsOpen(true);
@@ -62,21 +68,15 @@ export default function ProductCard() {
                   Pesan
                 </button>
               </div>
-            </section>
-          </Card>
+            </div>
+          </div>
         ))}
       </div>
       <Drawer
         open={isOpen}
         onClose={() => setIsOpen(false)}
         position={drawerPosition}
-        className="bg-linear-to-r from-[#243B55] to-[#141E30] text-white z-50 
-             /* 1. MOBILE: Muncul di bawah, tinggi 60% */
-             h-[60vh] rounded-t-2xl 
-             /* 2. TABLET (sm: 640px ke atas): Tetap di bawah, tinggi 45% */
-             sm:h-[45vh] 
-             /* 3. DESKTOP (lg: 1024px ke atas): Pindah ke kanan, tinggi penuh */
-             lg:h-full lg:w-4/12 lg:rounded-t-none lg:rounded-l-2xl"
+        className="bg-linear-to-r from-[#243B55] to-[#141E30] text-white z-50 h-[60vh] rounded-t-2xl sm:h-[45vh] lg:h-full lg:w-4/12 lg:rounded-t-none lg:rounded-l-2xl"
       >
         <DrawerItems>
           <BookingForm selectedService={selectedService} />
